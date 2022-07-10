72a1955fb1112251bc021eaaecf2b556555e3c09
hchier <1464002726@qq.com>
2022-07-10 16:58:41 +0800

    .

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px"></el-aside>
       <el-main>
         <div class="demo-collapse">
           <el-collapse accordion>
